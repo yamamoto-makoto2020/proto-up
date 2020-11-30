b0db15ad4d7c1fccc78a3d77778e2fb61391bca9
yamamoto-makoto2020 <start.2020@icloud.com>
2020-11-30 14:06:08 +0900

    画像詳細機能

---
+++ app/javascript/post.js
@@ -0,0 +1,24 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('img');
+
+    document.getElementById('post-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+        if (imageContent){
+          imageContent.remove();
+        }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+
+    });
+  });
+}
