2e4d519ca721ad0c0d0b0de288393f91b766319e
yamamoto-makoto2020 <start.2020@icloud.com>
2020-12-01 16:22:39 +0900

    画像編集機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_params, only: [:show, :edit, :update]
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
@@ -25,11 +26,26 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    if @post.save
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
   private
   def post_params
     params.require(:post).permit(:title, :text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @post = Post.find(params[:id])
   end
 end
